bedf6a7ae9782bfc64fd7cef2d93630838b1bc4a
the-algo <anujnmor@gmail.com>
2018-03-13 18:01:53 +0530

    Activate User

--- src/app/+dashboard/+activate-user/activate-user.component.ts
+++ src/app/+dashboard/+activate-user/activate-user.component.ts
@@ -8,9 +8,36 @@ import { FadeInTop } from "../../shared/animations/fade-in-top.decorator";
 })
 export class ActivateUserComponent implements OnInit {
 
+  userObject: UserModel[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.userObject.push(
+      { userId: 1, userName: "User1", emailId: "user1@gmail.com", status: "Active" },
+      { userId: 2, userName: "User2", emailId: "user2@gmail.com", status: "Active" },
+      { userId: 3, userName: "User3", emailId: "user3@gmail.com", status: "Inactive" },
+      { userId: 4, userName: "User4", emailId: "user4@gmail.com", status: "Inactive" },
+      { userId: 5, userName: "User5", emailId: "user5@gmail.com", status: "Inactive" },
+    )
   }
 
+  onClickActivate(user: UserModel) {
+    if (confirm("Are you sure, you want to " + user.status + " " + user.userName)) {
+      var id = this.userObject.indexOf(user);
+      if (this.userObject[id].status === "Inactive")
+        this.userObject[id].status = "Active";
+
+      else
+        this.userObject[id].status = "Inactive";
+    }
+  }
+
+}
+
+class UserModel {
+  userId: number;
+  userName: string;
+  emailId: string;
+  status: string;
 }
